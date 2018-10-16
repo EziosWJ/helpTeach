db0a7550b256251ccc70928cde495e379f2ba380
Wangj@ENVY STS <656876071@qq.com>
2018-10-16 17:10:36 +0800

    read json request

--- helpTeach/src/com/weixin/wj/controller/IndexController.java
+++ helpTeach/src/com/weixin/wj/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.weixin.wj.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jfinal.core.Controller;
+import com.jfinal.kit.HttpKit;
 import com.weixin.wj.model.Usercase;
 import com.weixin.wj.service.MenuService;
 import com.weixin.wj.service.impl.MenuServiceImpl;
@@ -21,7 +23,9 @@ public class IndexController extends Controller {
 	}
 	public void menu(){
 //		String role = ((Usercase)getSessionAttr("session_uc")).getUcRole();
-		renderJson(menuService.getMenuList("2"));
+		String readData = HttpKit.readData(getRequest());
+		String role = JSONObject.parseObject(readData).getString("role");
+		renderJson(menuService.getMenuList(role));
 //		renderJson(menuService.getMenuList(role));
 	}
 	
