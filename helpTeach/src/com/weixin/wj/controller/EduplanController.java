08a784a06d995075109fc6b85d1c06d5097ed673
Wangj@ENVY STS <656876071@qq.com>
2018-10-17 18:42:53 +0800

    add sql

--- helpTeach/src/com/weixin/wj/controller/EduplanController.java
+++ helpTeach/src/com/weixin/wj/controller/EduplanController.java
@@ -1,6 +1,5 @@
 package com.weixin.wj.controller;
 
-import java.util.List;
 
 import com.jfinal.core.Controller;
 
--- helpTeach/src/com/weixin/wj/controller/IndexController.java
+++ helpTeach/src/com/weixin/wj/controller/IndexController.java
@@ -3,11 +3,9 @@ package com.weixin.wj.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.jfinal.core.Controller;
 import com.jfinal.kit.HttpKit;
-import com.weixin.wj.model.Usercase;
 import com.weixin.wj.service.MenuService;
 import com.weixin.wj.service.impl.MenuServiceImpl;
 
-import nim.api.server.NIMService;
 
 public class IndexController extends Controller {
 	private MenuService menuService = new MenuServiceImpl();
